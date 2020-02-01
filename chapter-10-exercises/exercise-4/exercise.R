138c97487fbbfde182e966e07ec369a688b11e1e
gaganthind50 <gagant@uw.edu>
2020-01-31 21:21:06 -0800

    Completed exercise files for week 4

--- chapter-10-exercises/exercise-4/exercise.R
+++ chapter-10-exercises/exercise-4/exercise.R
@@ -5,9 +5,11 @@
 # Be sure to set your working directory in RStudio, and do NOT treat strings as 
 # factors!
 
+grants <- read.csv('data/gates_money.csv', stringsAsFactors = FALSE)
 
 # Use the View function to look at the loaded data
 
+View(grants)
 
 # Create a variable `organization` that contains the `organization` column of 
 # the dataset
@@ -31,9 +33,11 @@
 
 # Which organization received the largest grant?
 
+grants[grants$total_amount == max(grants$total_amount), "organization"]
 
 # Which organization received the smallest grant?
 
+grants[grants$total_amount == min(grants$total_amount), "organization"]
 
 # How many grants were awarded in 2010?
 
